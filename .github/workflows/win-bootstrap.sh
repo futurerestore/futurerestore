bcfe348f72af862a0210e5c97672cdd9e6f2cfaf
Cryptiiiic <liamwqs@gmail.com>
2022-10-09 03:32:52 -0700

    patch

--- .github/workflows/win-bootstrap.sh
+++ .github/workflows/win-bootstrap.sh
@@ -1,20 +1,15 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 set -e
-export WORKFLOW_ROOT=/Users/runner/work/futurerestore/futurerestore/.github/workflows
-export DEP_ROOT=/Users/runner/work/futurerestore/futurerestore/dep_root
-export BASE=/Users/runner/work/futurerestore/futurerestore/
+export WORKFLOW_ROOT=/d/a/futurerestore/futurerestore/.github/workflows
+export DEP_ROOT=/d/a/futurerestore/futurerestore/dep_root
+export BASE=/d/a/futurerestore/futurerestore
 
 cd ${WORKFLOW_ROOT}
 curl -sO https://cdn.cryptiiiic.com/deps/static/Windows/x86_64/Windows_x86_64_Release_Latest.tar.zst
 rm -rf ${DEP_ROOT}/{lib,include} || true
-mkdir -p ${DEP_ROOT}/macOS_x86_64_Release ${DEP_ROOT}/macOS_x86_64_Debug ${DEP_ROOT}/macOS_arm64_Release ${DEP_ROOT}/macOS_arm64_Debug
-gtar xf macOS_x86_64_Release_Latest.tar.zst -C ${DEP_ROOT}/macOS_x86_64_Release &
-gtar xf macOS_x86_64_Debug_Latest.tar.zst -C ${DEP_ROOT}/macOS_x86_64_Debug &
-gtar xf macOS_arm64_Release_Latest.tar.zst -C ${DEP_ROOT}/macOS_arm64_Release &
-gtar xf macOS_arm64_Debug_Latest.tar.zst -C ${DEP_ROOT}/macOS_arm64_Debug &
-wait
-sudo mv /usr/local/bin{,1}
+mkdir -p ${DEP_ROOT}/Windows_x86_64_Release
+tar xf Windows_x86_64_Release_Latest.tar.zst -C ${DEP_ROOT}/Windows_x86_64_Release
 cd ${BASE}
 git submodule update --init --recursive
 cd ${BASE}/external/tsschecker
---
+++ .github/workflows/win-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+export TMPDIR=/tmp
+export WORKFLOW_ROOT=/d/a/futurerestore/futurerestore/.github/workflows
+export DEP_ROOT=/d/a/futurerestore/futurerestore/dep_root
+export BASE=/d/a/futurerestore/futurerestore
+
+cd ${BASE}
+ln -sf ${DEP_ROOT}/Windows_x86_64_Release/{lib/,include/}  ${DEP_ROOT}/
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CROSSCOMPILING=false -DCMAKE_INSTALL_PREFIX=/tmp/out -DBUILD_SHARED_LIBS=0 -Wno-dev -DCMAKE_MAKE_PROGRAM=$(which make) -DCMAKE_C_COMPILER=$(which clang) -DCMAKE_MESSAGE_LOG_LEVEL="WARNING" -DCMAKE_CXX_COMPILER=$(which clang++) -G "CodeBlocks - Unix Makefiles" -S ./ -B cmake-build-release-x86_64 -DARCH=x86_64 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_LINKER=ld.lld -DNO_PKGCFG=1
+make -j4 -l4 -C cmake-build-release-x86_64
+
+#llvm-strip -s cmake-build-release-x86_64/src/futurerestore
